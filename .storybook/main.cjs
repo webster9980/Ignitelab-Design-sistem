6572a2f232ad2a7a64f779164c2c4a1916f4b2a0
webster ribeiro <webster96ribeiro@gmail.com>
2022-10-13 20:42:49 -0300

    ci: Add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configtype }) => {
+    if (configType === 'PRODUCTION'){
+      config.base = './Ignitelab-Design-sistem/'
+    }
+
+    return config
   }
 }
